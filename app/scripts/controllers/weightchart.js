9e1d138106e3c4f487837d00cdb1e04f66b41aff
fendy.g.santoso <fendy.g.santoso@accenture.com>
2014-04-22 12:10:11 -0700

    Integrate underscore.js, styling update, guideline implementation

--- app/scripts/controllers/weightchart.js
+++ app/scripts/controllers/weightchart.js
@@ -9,14 +9,14 @@ angular.module('weightwatcherClientApp')
         //X axis = timescale from first data date to today | Y axis weight from 150-220
         var chartConfig = {
           'data': $rootScope.weights,
-          'width': 700,
+          'width': 720,
           'height': 300,
-          'padding': 40
+          'padding': 30
         };
 
         //todochangerangetodynamic
-        chartConfig.x = d3.time.scale().domain([new Date(chartConfig.data[0].date), new Date()]).range([0, chartConfig.width]),
-        chartConfig.y = d3.scale.linear().domain([210,190]).rangeRound([0, chartConfig.height]);
+        chartConfig.x = d3.time.scale().domain([new Date(chartConfig.data[0].date), new Date()]).range([0, chartConfig.width-35]),
+        chartConfig.y = d3.scale.linear().domain([210,190]).range([0, chartConfig.height]);
 
         $('#weightChart').html('');
 
@@ -52,13 +52,15 @@ angular.module('weightwatcherClientApp')
         var xAxis = d3.svg.axis().
               scale(chartConfig.x).
               orient('bottom').
+              ticks(5).
               tickFormat(d3.time.format('%b %d')).
-              tickSize(1).
+              tickSize(-chartConfig.height).
               tickPadding(8);
 
         var yAxis = d3.svg.axis().
               scale(chartConfig.y).
               orient('left').
+              tickSize(-chartConfig.width).
               tickPadding(8);
 
         rootSVG.append('svg:g').
@@ -70,6 +72,38 @@ angular.module('weightwatcherClientApp')
           attr('class', 'y axis').
           attr('transform', 'translate('+chartConfig.padding+', '+chartConfig.padding+')').
           call(yAxis);
+
+        //Show guideline on mouseover
+        var guideLine = d3.select('#weightChart').append('div').
+              attr('class', '').
+              style('position', 'absolute').
+              style('z-index', '19').
+              style('width', '1px').
+              style('height', chartConfig.height - chartConfig.padding + 'px').
+              style('top', '0px').
+              style('margin-left', chartConfig.padding+'px').
+              style('background', '#333');
+
+        var dateArray = _.map($rootScope.weights, function(weight){
+          var tempDate = new Date(weight.date);
+          var month_date = tempDate.getMonth() +''+ tempDate.getDate();
+          return month_date;
+        });
+
+        rootSVG.on('mousemove', function(){
+          var mousex = d3.mouse(this)[0] - chartConfig.padding;
+          var mousex_invert = chartConfig.x.invert(mousex);
+          var hover_date = new Date(mousex_invert);
+          hover_date = hover_date.getMonth() +''+ hover_date.getDate();
+
+          var mapWeight = dateArray.indexOf(hover_date);
+          if(mapWeight!== -1) {
+            console.log(hover_date + '-' + $rootScope.weights[mapWeight].weight);
+          }
+
+          //Move guideline according to mouse position
+          guideLine.style('left', mousex + 'px');
+        });
       }
     };
   })
